cd2182dfe38fcb3b29c57bdd76c1f4ebb94f9688
matthew-groth <mgroth49@gmail.com>
2017-02-24 12:38:19 -0500

    please work

--- backgroundO.js
+++ backgroundO.js
@@ -1,19 +1,8 @@
-// Created by Matt Groth
-// Last updated 2/12/17
+//console.log('background script running!');
 
-// console.log('background script running!');
-
-function wait(ms)
-{
-var d = new Date();
-var d2 = null;
-do { d2 = new Date(); }
-while(d2-d < ms);
-}
-
-while(true) {
-    console.log('starting to wait');
-    wait(100);
-    console.log('wait over');
-    chrome.tabs.executeScript({file: "content_script.js"});
-}
+var intervalID = setInterval(function(){
+	//alert("Interval reached");
+	//console.log('wait over');
+	chrome.tabs.executeScript({file: "content_script.js", allFrames: true});
+	//console.log('starting to wait');
+}, 1000);
--- content_script.js
+++ content_script.js
@@ -1,7 +1,7 @@
 // Created by Matt Groth
 // Last updated 2/12/17
 
-// console.log('content script running!');
+//console.log('content script running - node-links should open new tabs!');
 
 var all = document.getElementsByClassName('node-link');
 for (var i=0, max=all.length; i < max; i++) {
